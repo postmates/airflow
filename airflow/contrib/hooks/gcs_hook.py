apache/spark

[AIRFLOW-4731] Fix GCS hook with google-storage-client 1.16 (#5368)
google-storage-client 1.16 introduced a breaking change where the
signature of client.get_bucket changed from (bucket_name) to
(bucket_or_name). Calls with named arguments to this method now fail.
This commit makes all calls positional to work around this.

airflow/contrib/hooks/gcs_hook.py
@@ -125,9 +125,9 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
             raise ValueError('source_bucket and source_object cannot be empty.')
 
         client = self.get_conn()
-        source_bucket = client.get_bucket(bucket_name=source_bucket)
+        source_bucket = client.get_bucket(source_bucket)
         source_object = source_bucket.blob(blob_name=source_object)
-        destination_bucket = client.get_bucket(bucket_name=destination_bucket)
+        destination_bucket = client.get_bucket(destination_bucket)
 
         token, bytes_rewritten, total_bytes = destination_bucket.blob(
             blob_name=destination_object).rewrite(
@@ -196,7 +196,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
                     filename = filename_gz
 
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket_name)
+        bucket = client.get_bucket(bucket_name)
         blob = bucket.blob(blob_name=object_name)
         blob.upload_from_filename(filename=filename,
                                   content_type=mime_type)
@@ -216,7 +216,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
         :type object_name: str
         """
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket_name)
+        bucket = client.get_bucket(bucket_name)
         blob = bucket.blob(blob_name=object_name)
         return blob.exists()
 
@@ -262,7 +262,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
         :type object_name: str
         """
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket_name)
+        bucket = client.get_bucket(bucket_name)
         blob = bucket.blob(blob_name=object_name)
         blob.delete()
 
@@ -286,7 +286,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
         :return: a stream of object names matching the filtering criteria
         """
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket_name)
+        bucket = client.get_bucket(bucket_name)
 
         ids = []
         pageToken = None
@@ -330,7 +330,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
                       object_name,
                       bucket_name)
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket_name)
+        bucket = client.get_bucket(bucket_name)
         blob = bucket.get_blob(blob_name=object_name)
         blob.reload()
         blob_size = blob.size
@@ -350,7 +350,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
         self.log.info('Retrieving the crc32c checksum of '
                       'object_name: %s in bucket_name: %s', object_name, bucket_name)
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket_name)
+        bucket = client.get_bucket(bucket_name)
         blob = bucket.get_blob(blob_name=object_name)
         blob.reload()
         blob_crc32c = blob.crc32c
@@ -370,7 +370,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
         self.log.info('Retrieving the MD5 hash of '
                       'object: %s in bucket: %s', object_name, bucket_name)
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket_name)
+        bucket = client.get_bucket(bucket_name)
         blob = bucket.get_blob(blob_name=object_name)
         blob.reload()
         blob_md5hash = blob.md5_hash

postmates/airflow

(cherry picked from commit 201e67100c200d38dbc3cd530cd04dd31bf91d7f)

airflow/contrib/hooks/gcs_hook.py
@@ -125,9 +125,9 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
             raise ValueError('source_bucket and source_object cannot be empty.')
 
         client = self.get_conn()
-        source_bucket = client.get_bucket(bucket_name=source_bucket)
+        source_bucket = client.get_bucket(source_bucket)
         source_object = source_bucket.blob(blob_name=source_object)
-        destination_bucket = client.get_bucket(bucket_name=destination_bucket)
+        destination_bucket = client.get_bucket(destination_bucket)
 
         token, bytes_rewritten, total_bytes = destination_bucket.blob(
             blob_name=destination_object).rewrite(
@@ -207,7 +207,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
                     filename = filename_gz
 
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket)
+        bucket = client.get_bucket(bucket)
         blob = bucket.blob(blob_name=object)
         blob.upload_from_filename(filename=filename,
                                   content_type=mime_type)
@@ -228,7 +228,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
         :type object: str
         """
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket)
+        bucket = client.get_bucket(bucket)
         blob = bucket.blob(blob_name=object)
         return blob.exists()
 
@@ -279,7 +279,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
             warnings.warn("'generation' parameter is no longer supported", DeprecationWarning)
 
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket)
+        bucket = client.get_bucket(bucket)
         blob = bucket.blob(blob_name=object)
         blob.delete()
 
@@ -346,7 +346,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
                       object,
                       bucket)
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket)
+        bucket = client.get_bucket(bucket)
         blob = bucket.get_blob(blob_name=object)
         blob.reload()
         blob_size = blob.size
@@ -366,7 +366,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
         self.log.info('Retrieving the crc32c checksum of '
                       'object: %s in bucket: %s', object, bucket)
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket)
+        bucket = client.get_bucket(bucket)
         blob = bucket.get_blob(blob_name=object)
         blob.reload()
         blob_crc32c = blob.crc32c
@@ -386,7 +386,7 @@ class GoogleCloudStorageHook(GoogleCloudBaseHook):
         self.log.info('Retrieving the MD5 hash of '
                       'object: %s in bucket: %s', object, bucket)
         client = self.get_conn()
-        bucket = client.get_bucket(bucket_name=bucket)
+        bucket = client.get_bucket(bucket)
         blob = bucket.get_blob(blob_name=object)
         blob.reload()
         blob_md5hash = blob.md5_hash